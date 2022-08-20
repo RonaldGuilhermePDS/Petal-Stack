4dc394508727abb7fb676c8f79b59dbc7c230922
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2022-08-20 20:55:10 -0300

    chore: add credo

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Petal.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
-      {:petal_components, "~> 0.17"}
+      {:petal_components, "~> 0.17"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
